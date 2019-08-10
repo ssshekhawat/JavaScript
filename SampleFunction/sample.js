c75158dabc523deef0d9000e546cbcca0b149284
Sandeep Singh Shekhawat <ssshekhawat@users.noreply.github.com>
2019-08-10 18:04:19 +0530

    Add files via upload

---
+++ SampleFunction/sample.js
@@ -0,0 +1,34 @@
+function welcome(name)
+{
+    alert("Hi! Welcome "+name+" to #100DaysofCode");
+}
+
+function carName(firstCar, secondCar)
+{
+    console.log(firstCar,secondCar);
+}
+
+function isChangeValue(country)
+{
+    country="USA";
+}
+
+let Person = {
+    FirstName:"Sandeep",
+    City:"Delhi"
+ }
+
+function changeDetail(person)
+{
+    person.FirstName="Ravindra"
+}
+
+function sum(firstNumber,secondNumber)
+{
+    let add= firstNumber+secondNumber;
+}
+function addition(firstNumber,secondNumber)
+{
+    let add= firstNumber+secondNumber;
+    return add;
+}
